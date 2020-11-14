0bb0dc8446a1403c2bb0590a90be90c23edab74a
Rafael Martín-Cuevas Redondo <rafamartinc@pm.me>
2020-11-14 18:19:45 +0100

    Search method refactored.

--- main.py
+++ main.py
@@ -40,13 +40,13 @@ def encode_credentials_in_base64(apikey: str, secret: str) -> str:
     return base64_credentials_str
 
 
-def login(base_url: str, apikey: str, secret: str) -> str:
+def login(base_url: str, content_type: str, apikey: str, secret: str) -> str:
 
     login_credentials = encode_credentials_in_base64(apikey, secret)
 
     url = base_url + '/oauth/token'
     response = requests.post(url, headers={
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        'Content-Type': content_type,
         'Authorization': 'Basic ' + login_credentials
     }, data={
         'scope': 'read',
@@ -56,30 +56,44 @@ def login(base_url: str, apikey: str, secret: str) -> str:
     return json.loads(response.content)['access_token']
 
 
+def search(base_url: str, content_type: str, access_token: str, country: str, operation: str,
+           property_type: str, latitude: float, longitude: float, distance: int,
+           order: str, sort: str, max_items: int, num_page: int) -> dict:
+
+    url = base_url + '/3.5/' + country + '/search'
+
+    response = requests.post(url, headers={
+        'Content-Type': content_type,
+        'Authorization': 'Bearer ' + access_token
+    }, data={
+        'operation': operation,
+        'propertyType': property_type,
+        'center': str(latitude) + ',' + str(longitude),
+        'distance': distance,
+        'order': order,
+        'sort': sort,
+        'maxItems': max_items,
+        'numPage': num_page
+    })
+
+    return json.loads(response.content)
+
+
 def main():
 
     base_url = 'http://api.idealista.com'
+    content_type = 'application/x-www-form-urlencoded;charset=UTF-8'
+
     args = process_args()
 
-    access_token = login(base_url, args.apikey, args.secret)
+    access_token = login(base_url=base_url, content_type=content_type,
+                         apikey=args.apikey, secret=args.secret)
 
-    country = 'es'
-    url = base_url + '/3.5/' + country + '/search'
-    response = requests.post(url, headers={
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        'Authorization': 'Bearer ' + access_token
-    }, data={
-        'operation': 'sale',
-        'propertyType': 'homes',
-        'center': '40.456176,-3.690273',
-        'distance': 900,
-        'order': 'distance',
-        'sort': 'asc',
-        'maxItems': 2,
-        'numPage': 1
-    })
-
-    results = json.loads(response.content)
+    results = search(base_url=base_url, content_type=content_type, access_token=access_token,
+                     country='es', operation='sale', property_type='homes',
+                     latitude=40.456176, longitude=-3.690273, distance=900,
+                     order='distance', sort='asc',
+                     max_items=2, num_page=1)
 
     print(json.dumps(results, indent=4, sort_keys=True))
 
